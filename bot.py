b8f92b6ab6d2fef9c66094fe7015c9563317307c
bataevaleksei <87965756+bataevaleksei@users.noreply.github.com>
2021-11-23 18:11:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from logging import exception
 import telebot
 import config
 import os
@@ -13,8 +14,9 @@ def welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Новый текст")
     item2 = types.KeyboardButton("Удалить")
+    item3 = types.KeyboardButton("Найти")
  
-    markup.add(item1, item2)
+    markup.add(item1, item2, item3)
  
     bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы сохранять сообщения.".format(message.from_user, bot.get_me()),
         parse_mode='html', reply_markup=markup)
@@ -23,15 +25,21 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def main(message):
     if message.chat.type == 'private':
+
+        # switch message.text
         if message.text == 'Новый текст':
             msg = bot.reply_to(message, 'Введите заголовок тeкста, который хотите добавить в систему')
             bot.register_next_step_handler(msg, new_head)
         elif message.text == 'Удалить':
             msg = bot.reply_to(message, 'Введите заголовок текста, который хотите удалить из системы')
             bot.register_next_step_handler(msg, delete_message)
+        elif message.text == 'Найти':
+            msg = bot.reply_to(message, 'Введите заголовок тeкста, который хотите найти')
+            bot.register_next_step_handler(msg, find_message)
         else:
             bot.reply_to(message, 'Я тебя не понимаю 😭')
 
+# get file_name
 def new_head(message):
     try:
         file_name = message.text + "_" + str(message.chat.id) + ".txt"
@@ -40,9 +48,11 @@ def new_head(message):
     except Exception:
         bot.reply_to(message, 'Не получилось сохранить сообщение: ' + message.text)
 
+# add message to the file
 def new_message(message, file_name):
     try:
         open(file_name, 'w').write(message.text)
+        bot.reply_to(message,'Данный текст успешно добавлен')
     except Exception:
         bot.reply_to(message,'Не получилось создать текст с заголовком: ' + message.text)
 
@@ -53,8 +63,14 @@ def delete_message(message):
         os.remove(path)
         bot.send_message(message.chat.id, "Текст с заголовком " + message.text + " успешно удалён!")
     except Exception:
-        bot.reply_to(message,"Не получилось удалить текст с заголовком: " + message.text + "\nВозможно такого файла не существует в системе")
+        bot.reply_to(message,"Не получилось удалить текст с заголовком: " + message.text + "\nВозможно такого заголовка не существует в системе")
         
- 
+def find_message(message):
+    try:
+        file_name= message.text + "_" + str(message.chat.id) + ".txt"
+        bot.send_message(message.chat.id, open(file_name, 'r').read())
+    except exception:
+        bot.send_message(message.chat.id, "Не удалось найти текст по заголовку: " + message.text +"\nВозможно такого заголовка не существует в системе")
+
 # RUN
 bot.polling(none_stop=True)
